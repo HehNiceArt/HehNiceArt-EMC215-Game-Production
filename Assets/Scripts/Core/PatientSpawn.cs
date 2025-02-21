e7ed5ef72b1646c709d38920247588fb9a6a9f08
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2025-02-22 03:45:17 +0800

    ye

--- Assets/Scripts/Core/PatientSpawn.cs
+++ Assets/Scripts/Core/PatientSpawn.cs
@@ -6,6 +6,7 @@ public class PatientSpawn : MonoBehaviour
     [SerializeField] GameObject[] patientPrefab;
     [SerializeField] Transform patientSpawnPoint;
     [SerializeField] float patientSpawnRate = 30f;
+    [SerializeField] float[] spawnWeights = new float[] { 60, 20, 10, 7, 3 };
 
     void Start()
     {
@@ -21,7 +22,26 @@ public class PatientSpawn : MonoBehaviour
     }
     void SpawnPatient()
     {
-        int rand = Random.Range(0, patientPrefab.Length);
-        Instantiate(patientPrefab[rand], patientSpawnPoint.position, Quaternion.identity);
+        float totalWeight = 0;
+        for (int i = 0; i < spawnWeights.Length; i++)
+        {
+            totalWeight += spawnWeights[i];
+        }
+
+        float randomPoint = Random.value * totalWeight;
+        float currentWeight = 0;
+
+        for (int i = 0; i < spawnWeights.Length; i++)
+        {
+            currentWeight += spawnWeights[i];
+            if (randomPoint <= currentWeight)
+            {
+                Instantiate(patientPrefab[i], patientSpawnPoint.position, Quaternion.identity);
+                return;
+            }
+        }
+
+        // Fallback in case of floating-point precision issues
+        Instantiate(patientPrefab[spawnWeights.Length - 1], patientSpawnPoint.position, Quaternion.identity);
     }
 }
