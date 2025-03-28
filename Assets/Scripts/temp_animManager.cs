b930656e19fc435f6f1d5bed5d3a7d0b1e9b79ac
sh1n-4 <sheene.sayaay@ciit.edu.ph>
2025-03-28 23:02:24 +0800

    states and transitions template

--- Assets/Scripts/temp_animManager.cs
+++ Assets/Scripts/temp_animManager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class temp_animManager : MonoBehaviour
 {
     private Animator anim;
+    private SpriteRenderer sr;
     private int currLVL = 0;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
         anim.SetBool("isIdle", true);
         anim.SetBool("facingFront", true);
     }
@@ -25,11 +27,20 @@ public class temp_animManager : MonoBehaviour
         {
             anim.SetBool("facingFront", true);
             Debug.Log("Facing Front.");
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            sr.flipX = false;
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            sr.flipX = true;
         }
+
+
 
         if (Input.GetKeyDown(KeyCode.F)) SetState("walk");
         else if (Input.GetKeyDown(KeyCode.G)) SetState("sit");
         else if (Input.GetKeyDown(KeyCode.H)) SetState("idle");
+        else if (Input.GetKeyDown(KeyCode.J)) SetState("working");
 
         if (Input.GetKeyDown(KeyCode.Alpha0)) SetLevel(0);
         else if (Input.GetKeyDown(KeyCode.Alpha1)) SetLevel(1);
@@ -51,11 +62,12 @@ public class temp_animManager : MonoBehaviour
     void SetState(string state)
     {
         anim.SetBool("isWalking", state == "walk");
-        Debug.Log(state == "walk");
+        //Debug.Log(state == "walk");
         anim.SetBool("isSitting", state == "sit");
-        Debug.Log(state == "sit");
+        //Debug.Log(state == "sit");
         anim.SetBool("isIdle", state == "idle");
-        Debug.Log(state == "idle");
+        //Debug.Log(state == "idle");
+        anim.SetBool("isWorking", state == "working");
         Debug.Log("State set to: " + state);
     }
 }
