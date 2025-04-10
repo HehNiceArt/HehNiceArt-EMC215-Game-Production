fbf22d580afb9f0aac4f6a0896c9cdf6f055819d
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2025-04-11 01:12:39 +0800

    double time and tut

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject tutorialUI;
+    [SerializeField] Sprite single, doubleTime;
+    [SerializeField] Button doubleTimeButton;
+
+    bool isActive = false;
+    bool isDoubleTime = false;
+
+    void Start()
+    {
+        StartCoroutine(TutShow());
+        doubleTimeButton.image.sprite = single;
+    }
+    IEnumerator TutShow()
+    {
+        tutorialUI.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        tutorialUI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isActive = !isActive;
+            StopCoroutine(TutShow());
+            tutorialUI.SetActive(isActive);
+        }
+    }
+    public void ShowTutorial()
+    {
+        isActive = !isActive;
+        tutorialUI.SetActive(isActive);
+    }
+
+    public void DoubleTme()
+    {
+        isDoubleTime = !isDoubleTime;
+
+        if (isDoubleTime)
+        {
+            doubleTimeButton.image.sprite = doubleTime;
+            Time.timeScale = 2;
+        }
+        else
+        {
+            doubleTimeButton.image.sprite = single;
+            Time.timeScale = 1;
+        }
+    }
+}
